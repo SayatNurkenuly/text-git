0f394b1b0ce206aafd72a3dd24977c4a74be07bb
Sayat <nurkenulysayat@gmail.com>
2024-10-21 13:11:39 +0500

    python basics.py

---
+++ Assigment6/python basics.py
@@ -0,0 +1,112 @@
+#1
+# a = 5
+# b = 6
+# print(a+b)
+
+#2
+# name = "sayat"
+# reversed_name = name[::-1]
+# print(reversed_name)
+# print(name)
+
+#3
+# string = "kanagattandyrilmagandyktarynyzdan"
+# print(len(string))
+
+#4
+# string1 = "Nurkenuly "
+# string2 = "Sayat"
+#
+# print(string1 + string2)
+
+#5
+# character = input('write: ')
+# if character.lower() in 'sayat':
+#     print(character + " is vowel")
+# else:
+#     print(character + " is not vowel")
+
+#6
+# string = 'Nurkenuly'
+# if len(string) > 1:
+#     mstring = string[-1] + string [1:-1] + string[0]
+# else:
+#     mstring = string
+# print(mstring)
+
+#7
+# string = 'nfactorial'
+# uppercase_string = string.upper()
+# print(uppercase_string)
+
+#8
+# a = 15
+# b = 2
+# s = a*b
+# print(s)
+
+#9
+# a = int(input())
+#
+# if a % 2 == 0:
+#     print("even")
+# else:
+#     print("not even")
+
+#10
+# string = "Nurkenuly"
+# mstring = string[:3]
+# print(mstring)
+
+#11
+# name = 'Sayat'
+# old = 20
+# message = 'Hello, my name is {name} and I am {old} years old'
+# print(message)
+
+#12
+# string = 'Nurkenuly'
+# mstring = string[2:5]
+# print(mstring)
+
+#13
+# str_sum = 2024
+# print(str_sum)
+
+#14
+# string = 'Sayat'
+# i = 0
+# while i<3:
+#     print(string)
+#     i+=1
+
+#15
+# a = int(input())
+# b = int(input())
+# sum = a/b
+# sum1 = a%b
+# print(sum)
+# print(sum1)
+
+#16
+# a = float(input())
+# b = float(input())
+# sum = a/b
+# print(sum)
+
+#17
+# a = 'Hello, How are you?'
+# b = 'You?'
+# print(f"{a}  Good. {b} I am good " )
+
+#19
+# print('1 string \n2 string \n3 string')
+
+#20
+# a = int(input())
+# b = int(input())
+# print(a**b)
+
+
+
+
